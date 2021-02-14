e7590f7552db7f67823c18b0713c49374d177a76
philip-long <p.long@northeastern.edu>
2021-02-14 14:20:43 +0000

    added cable robot

--- polytope_functions.py
+++ polytope_functions.py
@@ -3,6 +3,14 @@ import polytope
 import matplotlib.pyplot as plt
 from scipy.spatial import Delaunay, ConvexHull
 
+def get_Cartesian_polytope(jacobian,joint_space_vrep):
+
+
+    Pv=np.zeros([np.shape(joint_space_vrep)[0],np.shape(jacobian)[0]])
+
+    for row,i in zip(joint_space_vrep,range(np.shape(joint_space_vrep)[0])):
+        Pv[i,:]=np.matmul(jacobian,row)
+    return Pv
 
 def plot_polytope_3d(poly):
     V = polytope.extreme(poly)
@@ -18,3 +26,9 @@ def plot_polytope_3d(poly):
 
     plt.show()
     return ax
+
+def point_in_hull(point, hull, tolerance=1e-12):
+    #    https: // stackoverflow.com / questions / 16750618 / whats - an - efficient - way - to - find - if -a - point - lies - in -the - convex - hull - of - a - point - cl / 42165596  # 42165596
+    return all(
+        (np.dot(eq[:-1], point) + eq[-1] <= tolerance)
+        for eq in hull.equations)
